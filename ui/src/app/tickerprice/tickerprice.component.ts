dc252814cf22550788d1d0a2dbcb47ce0505dfd1
Cadu Grillo <cadugrillo@gmail.com>
2022-05-04 17:17:39 -0400

    bug fixing

--- ui/src/app/tickerprice/tickerprice.component.ts
+++ ui/src/app/tickerprice/tickerprice.component.ts
@@ -20,13 +20,9 @@ export class TickerpriceComponent implements OnInit {
 getTickerPrice() {
 
   this.tickersMod = this.tickers.replace(/,/g,"%2C");
-  console.log(this.tickersMod);
-
+  
   this.tickerpriceService.getTickerPrice(this.tickers).subscribe((data) => {
-    console.log(data);
     this.response = (data as Tickers);
-    console.log(this.response);
-    console.log(this.response.data[0].id);
   });
 
 }

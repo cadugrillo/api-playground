f8c1db54c937cddaf2ee0fcf18fe819a5f353674
Cadu Grillo <cadugrillo@gmail.com>
2022-05-04 21:59:58 -0400

    added CSS styles

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatefactComponent } from './datefact/datefact.component';
+import { TickerpriceComponent } from './tickerprice/tickerprice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'dayfacts',component: DatefactComponent},
+  { path: 'tickerprice',component: TickerpriceComponent},
+  { path: '**', redirectTo: 'dayfacts'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
